d3dfc548dbbc6da383f962836d8538ca4a0c200e
Zar <die.rubioa@duocuc.cl>
2026-05-12 13:10:19 -0400

    Update SaludosP.py
    cambiado y corregido el contador dando el numero de participante al ingresar el nombre

--- SaludosP.py
+++ SaludosP.py
@@ -1,13 +1,20 @@
 CantParticipantes = int(input("Ingrese la cantidad de participantes: "))
-
 nombres = []
 
+## Guardar Nombres ##
 for i in range(CantParticipantes):
     nombre = input("Ingrese el nombre del participante: ")
     nombres.append(nombre)
 
+## Saludar participantes ##
+contador = 1
 for nombre in nombres:
-    print(f"Bienvenido/a, {nombre}")
+    print(f"Bienvenido/a numero {contador}, {nombre}")
+    contador += 1
+
+## Mensaje final
+print(f"Capacitacion inicializada. Total de participantes saludados: {CantParticipantes}")
+
 
 
 ## 
